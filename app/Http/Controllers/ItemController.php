4f7ea366b55fb30658e1c4b56fa5ba98d692ae8b
Randy <randy@leetdigital.com>
2020-07-19 13:21:30 +0800

    Update ItemController.php

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -241,7 +241,7 @@ class ItemController extends Controller
     }
 
     public function getHeadwears() {
-        return Item::whereIn('type_name', $this->headwears__)->orderBy('id', 'desc')->paginate();
+        return Item::whereIn('type_name', $this->headwears__)->orderBy('name_en', 'asc')->paginate();
     }
 
     public function getHeadwear($id) {
